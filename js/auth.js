92a2c91d4d7354f3179d9fd2d25a4186b3f0474d
philosofonusus <philosofmagic7@gmail.com>
2018-03-01 10:29:07 +0530

    new commit

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 function au() {
     pass = prompt('Введите пароль');
     if (pass=='1314$%$z.?') {}
-    else { alert('Неверный пароль!'), top.location.href="" }
+    else { top.location.href="" }
 }
